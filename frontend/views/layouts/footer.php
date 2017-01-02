67ba72ed3bf309aee86264b32b8c732ddcec8bdd
Dalia Atef <dalia@digitreeinc.com>
2017-01-02 13:08:21 +0200

    social_links

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -4,10 +4,10 @@
             <div class="copyRights"><span><a target="_blank" href="//digitreeinc.com">&copy; Hype 2016 powered By Digitree.</a></span></div>
             <div class="social-links clearfix">
                 <span>Follow us </span>
-                <div class="social-link youTube"></div>
-                <div class="social-link twit"></div>
-                <div class="social-link inst"></div>
-                <div class="social-link fb"></div>
+                <!--<div class="social-link youTube"></div>-->
+                <div class="social-link twit"><a href="https://twitter.com/HypeInsights/" target="_blank"></a></div>
+                <div class="social-link inst"><a href="https://www.instagram.com/hypeinsights/" target="_blank"></a></div>
+                <div class="social-link fb"><a href="https://www.facebook.com/Hype-Insights-713855192098064/?fref=ts" target="_blank"></a></div>
             </div>
 	</div>
     </div>
